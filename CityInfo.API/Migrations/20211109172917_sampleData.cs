bdadb96a6a1c04cddbccfd1c1fb9467101cebf50
cj.green <cj.green@intimetec.com>
2021-11-09 10:30:42 -0700

    fixing connection db issues

---
+++ CityInfo.API/Migrations/20211109172917_sampleData.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CityInfo.API.Migrations
+{
+    public partial class sampleData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, "The one with that big park.", "New York City" },
+                    { 2, "The one with the cathedral that was never really finished.", "Antwerp" },
+                    { 3, "The one with that big tower.", "Paris" },
+                    { 4, "The one where everybody is moving to. Don't come here, it's a desert. It's ugly. Please, I beg you not to come here.", "Boise" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "PointOfInterests",
+                columns: new[] { "Id", "CityId", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "The most visited urban park in th e United States.", "Central Park" },
+                    { 2, 1, "A 102-story skyscraper located in Midtown Manhattan.", "Empire State Building" },
+                    { 3, 2, "A Gothic style cathedral, conceived by architects Jan and Pierre", "Cathedral of Our Lady" },
+                    { 4, 2, "The finest example of railway architecture in Belgium.", "Antwerp Central Station" },
+                    { 5, 3, "A wrought iron lattice tower on the Champ de Mars.", "Eiffel Tower" },
+                    { 6, 3, "The world's largest museum.", "The Louvre" },
+                    { 7, 4, "Some say you can find ghosts inside.", "Old Idaho State Penitentiary" },
+                    { 8, 4, "Located in the heart of downtown Boise. An interesting culture indeed.", "The Basque Musuem & Cultural Center" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
